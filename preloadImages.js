8760dabeefdc6f7159c8cd5da17d76b7d8832833
squidwardschair <hudsonzhao1@gmail.com>
2024-11-07 21:04:59 -0500

    next gen imgs

--- preloadImages.js
+++ preloadImages.js
@@ -1,4 +1,4 @@
-let images = ["imgs/aboutusimg.webp", "imgs/bari.webp", "imgs/catherine.jpg", "imgs/cello.jpg", "imgs/cello.jpg", "imgs/discord.svg", "imgs/flute.jpeg", "imgs/followus.webp", "imgs/ig.webp", "imgs/iris.jpg", "imgs/joinus.webp", "imgs/linktree.webp", "imgs/online.jpg", "imgs/scheduleimg.webp"]
+let images = ["imgs/aboutusimg.webp", "imgs/bari.webp", "imgs/catherine.avif", "imgs/cello.avif", "imgs/discord.svg", "imgs/flute.avif", "imgs/followus.webp", "imgs/ig.webp", "imgs/iris.avif", "imgs/joinus.webp", "imgs/linktree.webp", "imgs/online.avif", "imgs/scheduleimg.webp"]
 
 for (i of images) {
     let img = new Image()
@@ -6,7 +6,7 @@ for (i of images) {
 }
 
 for (let e = 1; e<12; e++) {
-    let fp = `imgs/team/${e}.jpg`
+    let fp = `imgs/team/${e}.avif`
     let img = new Image()
     img.src=fp
 }
